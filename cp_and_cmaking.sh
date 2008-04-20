f7108ff2b2981dde4841803afa3f355558ccddfd
Alejandro Leiva <gloob@php.net>
2008-04-20 19:15:47 +0000

    [alpha]: Initial draft of CMake architecture for PHP.

---
+++ cp_and_cmaking.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+#
+# $Id$
+
+if [ ! -n "$1" ]
+then
+  echo "Usage: `basename $0` <dir>."
+  exit 65
+fi  
+
+cp -r cmake/* $1
+
+rm -rf build
+mkdir -p build
+cd build && cmake ../$1 && make

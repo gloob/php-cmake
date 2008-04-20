1fee00f4a086ab1fcf24126a558aff79dced0e21
Alejandro Leiva <gloob@php.net>
2008-04-20 23:07:07 +0000

    [main]: Compiling main section.

--- cp_and_cmaking.sh
+++ cp_and_cmaking.sh
@@ -10,6 +10,6 @@ fi
 
 cp -r cmake/* $1
 
-rm -rf build
-mkdir -p build
-cd build && cmake ../$1 && make
+#rm -rf build
+#mkdir -p build
+cd build && cmake -DCMAKE_BUILD_TYPE:STRING=Debug ../$1 && make

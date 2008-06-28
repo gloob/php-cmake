ac106f7460fb5508b617c228bd3051c84e4c0b47
Alejandro Leiva <gloob@php.net>
2008-06-28 23:12:20 +0000

    Added version checking to FindRE2C and draft sapi skel.

--- cmake/CMake/FindRE2C.cmake
+++ cmake/CMake/FindRE2C.cmake
@@ -3,6 +3,7 @@
 #
 #  RE2C_FOUND - system has re2c
 #  RE2C_EXECUTABLE - the re2c executable path
+#  RE2C_VERNUM - version of re2c
 #
 #  Copyright (c) 2008 Alejandro Leiva <gloob@php.net>
 #
@@ -20,6 +21,17 @@ mark_as_advanced(RE2C_EXECUTABLE)
 if(RE2C_EXECUTABLE)
   set(RE2C_FOUND TRUE)
   # check version
+  execute_process(COMMAND ${RE2C_EXECUTABLE} --vernum
+    RESULT_VARIABLE RE2C_version_result
+    OUTPUT_VARIABLE RE2C_version_output
+    ERROR_VARIABLE RE2C_version_error
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+#  message("RE2C_version_result:${RE2C_version_result} RE2C_version_output:${RE2C_version_output} RE2C_version_error:${RE2C_version_error}")
+  if(RE2C_version_result EQUAL 0)
+    message(SEND_ERROR "Command \"${RE2C_EXECUTABLE} --vernum\" failed with output:\n${RE2C_version_error}")
+  else(RE2C_version_result EQUAL )
+    set(RE2C_VERNUM ${RE2C_version_output})
+  endif(RE2C_version_result EQUAL 0)
 
   #set call macro
   macro(RE2C_TARGET Input Output Args)

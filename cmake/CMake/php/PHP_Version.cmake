f74c2cf41c22e3551b8e31bad189d1fe473cfd06
Alejandro Leiva <gloob@php.net>
2008-07-21 10:10:34 +0000

    Redefined PHP_Version CMake script. Some includes added.

--- cmake/CMake/php/PHP_Version.cmake
+++ cmake/CMake/php/PHP_Version.cmake
@@ -10,33 +10,39 @@
 
 message("* Start of Version checking.")
 
-find_file(_configure_in_filepath "${CMAKE_CURRENT_SOURCE_DIR}/configure.in")
-file(READ ${_configure_in_filepath} _configure_in_contents)
+find_file(_configure_in_filepath configure.in PATHS ${PHP_SOURCE_DIR})
+message("_configure_in_filepath: ${_configure_in_filepath} ${PHP_SOURCE_DIR}/configure.in")
 
-string(REGEX MATCH "PHP_MAJOR_VERSION=([0-9]*)" _match_major_version "${_configure_in_contents}")
-string(REGEX MATCH "PHP_MINOR_VERSION=([0-9]*)" _match_minor_version "${_configure_in_contents}")
-string(REGEX MATCH "PHP_RELEASE_VERSION=([0-9]*)" _match_release_version "${_configure_in_contents}")
-string(REGEX MATCH "PHP_EXTRA_VERSION=\"([^\"]*)\"" _match_extra_version "${_configure_in_contents}")
+if(_configure_in_filepath)
 
-string(REPLACE "PHP_MAJOR_VERSION=" "" match_major_version ${_match_major_version}) 
-string(REPLACE "PHP_MINOR_VERSION=" "" match_minor_version ${_match_minor_version})
-string(REPLACE "PHP_RELEASE_VERSION=" "" match_release_version ${_match_release_version})
-string(REPLACE "PHP_EXTRA_VERSION=" "" match_extra_version ${_match_extra_version})
-string(REPLACE "\"" "" match_extra_version ${match_extra_version})
+  file(READ ${_configure_in_filepath} _configure_in_contents)
 
-set(PHP_MAJOR_VERSION ${match_major_version})
-set(PHP_MINOR_VERSION ${match_minor_version})
-set(PHP_RELEASE_VERSION ${match_release_version})
-set(PHP_EXTRA_VERSION ${match_extra_version})
+  string(REGEX MATCH "PHP_MAJOR_VERSION=([0-9]*)" _match_major_version "${_configure_in_contents}")
+  string(REGEX MATCH "PHP_MINOR_VERSION=([0-9]*)" _match_minor_version "${_configure_in_contents}")
+  string(REGEX MATCH "PHP_RELEASE_VERSION=([0-9]*)" _match_release_version "${_configure_in_contents}")
+  string(REGEX MATCH "PHP_EXTRA_VERSION=\"([^\"]*)\"" _match_extra_version "${_configure_in_contents}")
 
-set(PHP_VERSION "${PHP_MAJOR_VERSION}.${PHP_MINOR_VERSION}.${PHP_RELEASE_VERSION}${PHP_EXTRA_VERSION}")
-math(EXPR PHP_VERSION_ID ${PHP_MAJOR_VERSION}*10000+${PHP_MINOR_VERSION}*100+${PHP_RELEASE_VERSION})
+  string(REPLACE "PHP_MAJOR_VERSION=" "" match_major_version ${_match_major_version}) 
+  string(REPLACE "PHP_MINOR_VERSION=" "" match_minor_version ${_match_minor_version})
+  string(REPLACE "PHP_RELEASE_VERSION=" "" match_release_version ${_match_release_version})
+  string(REPLACE "PHP_EXTRA_VERSION=" "" match_extra_version ${_match_extra_version})
+  string(REPLACE "\"" "" match_extra_version ${match_extra_version})
 
-message("PHP_MAJOR_VERSION: ${PHP_MAJOR_VERSION}")
-message("PHP_MINOR_VERSION: ${PHP_MINOR_VERSION}")
-message("PHP_RELEASE_VERSION: ${PHP_RELEASE_VERSION}")
-message("PHP_EXTRA_VERSION: ${PHP_EXTRA_VERSION}")
-message("PHP_VERSION: ${PHP_VERSION}")
-message("PHP_VERSION_ID: ${PHP_VERSION_ID}")
+ set(PHP_MAJOR_VERSION ${match_major_version})
+ set(PHP_MINOR_VERSION ${match_minor_version})
+ set(PHP_RELEASE_VERSION ${match_release_version})
+ set(PHP_EXTRA_VERSION ${match_extra_version})
+
+ set(PHP_VERSION "${PHP_MAJOR_VERSION}.${PHP_MINOR_VERSION}.${PHP_RELEASE_VERSION}${PHP_EXTRA_VERSION}")
+ math(EXPR PHP_VERSION_ID ${PHP_MAJOR_VERSION}*10000+${PHP_MINOR_VERSION}*100+${PHP_RELEASE_VERSION})
+
+ message("PHP_MAJOR_VERSION: ${PHP_MAJOR_VERSION}")
+ message("PHP_MINOR_VERSION: ${PHP_MINOR_VERSION}")
+ message("PHP_RELEASE_VERSION: ${PHP_RELEASE_VERSION}")
+ message("PHP_EXTRA_VERSION: ${PHP_EXTRA_VERSION}")
+ message("PHP_VERSION: ${PHP_VERSION}")
+ message("PHP_VERSION_ID: ${PHP_VERSION_ID}")
+
+endif(_configure_in_filepath)
 
 message("* Stop of Version checking.")
